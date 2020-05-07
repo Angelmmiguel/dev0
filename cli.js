32afbb2e51b28488b82ee51ad36a912416ce99b4
Angel M <Angelmmiguel@users.noreply.github.com>
2020-05-07 23:57:09 +0200

    Create the initial CLI and server

---
+++ cli.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+const dev0 = require(".");
+
+// CLI arguments
+const availableArgs = [
+  {
+    name: "port",
+    defaultValue: dev0.defaultPort,
+    description: "Port to use",
+    alias: [
+      "--port",
+      "-p"
+    ],
+    validate: v => !isNaN(v),
+    parse: v => parseInt(v)
+  },
+  {
+    name: "help",
+    description: "Display this help",
+    alias: [
+      "--help",
+      "-h"
+    ]
+  }
+]
+
+// dev0 arguments
+const folders = [];
+const options = {};
+
+// Set default values
+availableArgs.forEach(arg => {
+  if (arg.defaultValue != null) {
+    options[arg.name] = arg.defaultValue
+  }
+});
+
+// Process the arguments
+const args = process.argv.slice(2);
+let currentArg;
+
+args.forEach(arg => {
+  const argType = availableArgs.find(availableArg => availableArg.alias.includes(arg));
+
+  if (currentArg == null && argType != null) {
+    currentArg = argType;
+  } else if (currentArg != null) {
+    if (currentArg.validate(arg)) {
+      options[currentArg.name] = typeof currentArg.parse === "function" ? currentArg.parse(arg) : arg;
+    } else {
+      console.log(`Error! ${arg} is not a valid value for ${currentArg.name}`);
+    }
+    currentArg = null
+  } else if (arg.startsWith("-")) {
+    console.log(`Error! ${arg} is not a valid argument.`);
+  } else {
+    folders.push(arg);
+  }
+});
+
+dev0(folders, options);
---
+++ index.js
@@ -0,0 +1,88 @@
+// NodeJS modules
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+// Common mime types
+const mimeTypes = {
+  ".html": "text/html",
+  ".jpeg": "image/jpeg",
+  ".jpg": "image/jpeg",
+  ".png": "image/png",
+  ".js": "text/javascript",
+  ".css": "text/css"
+};
+
+// Default values
+const DEFAULT_PORT = 3000;
+const DEFAULT_HOST = 'localhost';
+const DEFAULT_ENCODING = 'utf8';
+
+const filesByUrl = (url) => {
+  if (url.endsWith("/")) {
+    return [
+      `${url}index.html`
+    ];
+  } else {
+    return [
+      `${url}`,
+      `${url}.html`,
+      `${url}/index.html`,
+    ];
+  }
+};
+
+const dev0 = (folders, { port }) => {
+  const listenPort = port || DEFAULT_PORT;
+
+  const server = http.createServer((req, res) => {
+    const { url } = req;
+    const filesToTry = filesByUrl(url);
+    let file;
+
+    filesToTry.forEach((f) => {
+      folders.forEach(folder => {
+        const filePath = path.isAbsolute(folder) ? path.join(folder, f) : path.join(__dirname, folder, f);
+        if (file == null && fs.existsSync(filePath)) {
+          file = filePath;
+        }
+      });
+    });
+
+    if (file == null) {
+      res.writeHead(404);
+      res.end(`
+        <h1>Not Found</h1>
+        <p>Tried files:</p>
+        <ul>
+          ${filesToTry.map((f) => `<li>${f}</li>`)}
+        </ul>
+      `, DEFAULT_ENCODING);
+    } else {
+      const readStream = fs.createReadStream(file);
+      const mimeType = mimeTypes[path.extname(file)] || "text/plain";
+
+      readStream.on('open', () => {
+        res.writeHead(200, { "Content-Type": `${mimeType}; charset=${DEFAULT_ENCODING}` });
+        readStream.pipe(res);
+      });
+    
+      readStream.on('error', (err) => {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end(err, DEFAULT_ENCODING);
+      });
+    }
+
+  });
+
+  // Started!
+  console.log(`Listening on http://${DEFAULT_HOST}:${listenPort}`);
+
+  server.listen(listenPort);
+};
+
+// Store default values
+dev0.defaultPort = DEFAULT_PORT;
+
+// Export the library
+module.exports = dev0;
