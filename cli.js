3d258878b22fcf94cbda8f847259be2b504a0e93
Angel M <Angelmmiguel@users.noreply.github.com>
2020-05-08 23:04:35 +0200

    Add help information to the CLI

--- cli.js
+++ cli.js
@@ -8,8 +8,8 @@ const availableArgs = [
     defaultValue: dev0.defaultPort,
     description: "Port to use",
     alias: [
-      "--port",
-      "-p"
+      "-p",
+      "--port"
     ],
     validate: v => !isNaN(v),
     parse: v => parseInt(v)
@@ -17,12 +17,34 @@ const availableArgs = [
   {
     name: "help",
     description: "Display this help",
+    bool: true,
     alias: [
-      "--help",
-      "-h"
+      "-h",
+      "--help"
     ]
   }
 ];
+
+const showHelp = () => {
+  console.log("Description:")
+  console.log("  A zero-dependency static file server for development");
+  console.log("\nUsage:");
+  console.log("  dev0 [options] <folders>");
+  console.log("\nOptions:");
+  availableArgs.forEach(availableArg => {
+    let out = `  ${availableArg.alias.join(", ")}\t${availableArg.description}`;
+
+    if (availableArg.defaultValue != null) {
+      out += ` (Default: ${availableArg.defaultValue})`;
+    }
+    console.log(`${out}`);
+  });
+  console.log("\nExamples:");
+  console.log("  dev0 blog");
+  console.log("  dev0 -p 8080 blog");
+  console.log("  dev0 blog build");
+  console.log("");
+};
 
 // dev0 arguments
 const folders = [];
@@ -43,7 +65,11 @@ args.forEach(arg => {
   const argType = availableArgs.find(availableArg => availableArg.alias.includes(arg));
 
   if (currentArg == null && argType != null) {
-    currentArg = argType;
+    if (argType.bool === true) {
+      options[argType.name] = true;
+    } else {
+      currentArg = argType;
+    }
   } else if (currentArg != null) {
     if (currentArg.validate(arg)) {
       options[currentArg.name] = typeof currentArg.parse === "function" ? currentArg.parse(arg) : arg;
@@ -58,5 +84,16 @@ args.forEach(arg => {
     folders.push(arg);
   }
 });
+
+if (options.help === true) {
+  showHelp();
+  process.exit(0);
+}
+
+if (folders.length === 0) {
+  console.log("[ERROR] You should define at least one folder\n");
+  showHelp();
+  process.exit(1);
+}
 
 dev0(folders, options);
